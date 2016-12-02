56dcb4abb3e2fbacb0f530c0dd4aa64699685f02
John Elliott <johnelliott703@gmail.com>
2016-12-02 11:45:52 -0500

    Update installed apps

--- brew-cask.sh
+++ brew-cask.sh
@@ -8,6 +8,7 @@ brew install caskroom/cask/brew-cask
 brew cask install google-chrome 2> /dev/null
 brew cask install google-chrome-canary 2> /dev/null
 brew cask install iterm2 2> /dev/null
+brew cask install flux 2> /dev/null
 brew cask install vlc 2> /dev/null
 brew cask install spectacle 2> /dev/null
 brew cask install slack 2> /dev/null
