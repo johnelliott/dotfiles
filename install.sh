3ca9ff55f7ccee9b7dbce1d6c2e94a9688d20f83
John Elliott <johnelliott703@gmail.com>
2016-12-23 00:35:58 -0500

    Add top-level install script

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE}")" || exit
+
+#function removeOldFiles() {
+#	for file in ~/.{aliases,bash_profile,bash_prompt,bashrc,curlrc,exports,gitconfig,gitignore,hushlogin,inputrc,path,tmux.conf,wgetrc,zshrc,hub-completion.sh,git-completion.sh}; do
+#		rm "$file" >/dev/null 2>&1
+#	done
+#}
+#
+#function copyNewFiles() {
+#	cp .bash_profile ~/.bash_profile
+#	cp .bashrc ~/.bashrc
+#	cp .vim ~/.vimrc
+#	ln -s .gitconfig ~/.gitconfig
+#}
+#
+#function doIt() {
+#	removeOldFiles
+#	copyNewFiles
+#	source ~/.bash_profile
+#}
+#
+#if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+#	doIt
+#else
+#	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1 -r
+#	echo
+#	if [[ $REPLY =~ ^[Yy]$ ]]; then
+#		doIt
+#	fi
+#fi
+
+# Install Homebrew on OS X
+if [[ $(uname) == 'Darwin' ]]; then
+	read -p "Would you like to set up Homebrew? (y/n) " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		./brew.sh
+	fi
+
+	read -p "Would you like to set up OS X preferences? (y/n) " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		./osx.sh
+	fi
+fi
+
+# Install npm packages
+if which npm > /dev/null; then
+	read -p "Would you like to install npm packages? (y/n) " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		./npm.sh
+	fi
+fi
