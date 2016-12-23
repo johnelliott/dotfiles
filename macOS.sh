b083f12fffe8bbe8d21272eede7511b0e488fc4b
John Elliott <johnelliott703@gmail.com>
2016-12-23 00:34:10 -0500

    Update preferences for new machines

---
+++ macOS.sh
@@ -0,0 +1,109 @@
+#!/usr/bin/env bash
+
+# ~/.osx — https://mths.be/osx
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# General UI/UX
+# power plug chime
+defaults write com.apple.PowerChime ChimeOnAllHardware -bool true
+
+# Set computer name (as done via System Preferences → Sharing)
+sudo scutil --set ComputerName "duffie"
+sudo scutil --set HostName "duffie"
+sudo scutil --set LocalHostName "duffie"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "duffie"
+
+# Dark mode and reduce transparency
+defaults write .GlobalPreferences AppleInterfaceStyle -string "Dark"
+defaults write com.apple.universalaccess reduceTransparency -int 1
+
+# Set highlight color to hi-viz green
+defaults write NSGlobalDomain AppleHighlightColor -string "0.847911 1.000000 0.000000"
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -int 1
+defaults write com.apple.universalaccess closeViewPanningMode -int 0
+
+# Expand save panel by default
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expand print panel by default
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Save to disk (not to iCloud) by default
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Set Help Viewer windows to non-floating mode
+defaults write com.apple.helpviewer DevMode -bool true
+
+# Disable press-and-hold for keys in favor of key repeat
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+
+# Finder
+# Finder: allow quitting via ⌘ + Q; doing so will also hide desktop icons
+#defaults write com.apple.finder QuitMenuItem -bool true
+
+# Set up desktop icon view
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy kind" ~/Library/Preferences/com.apple.finder.plist
+#/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:gridSpacing 100" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:labelOnBottom 0" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo 0" ~/Library/Preferences/com.apple.finder.plist
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:iconSize 16" ~/Library/Preferences/com.apple.finder.plist
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Finder: allow text selection in Quick Look
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+# Display full POSIX path as Finder window title
+#defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+# When performing a search, search the current folder by default
+#defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Enable AirDrop over Ethernet and on unsupported Macs running Lion
+#defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# Allow hitting the Backspace key to go to the previous page in history
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled" -bool true
+defaults write com.apple.safari Command1Through9SwitchesTabs -int 1
+defaults write com.apple.safari IncludeDevelopMenu -int 1
+defaults write com.apple.safari SendDoNotTrackHTTPHeader -int 1
+defaults write com.apple.safari ShowFullURLInSmartSearchField -int 1
+defaults write com.apple.safari ShowOverlayStatusBar -int 1
+defaults write com.apple.safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -int 1
+defaults write com.apple.safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2TabsToLinks" -int 1
+#defaults write com.apple.safari NewWindowBehavior -int 4
+#defaults write com.apple.safari TabCreationPolicy -int 0
+#defaults write com.apple.safari WebKitDeveloperExtrasEnabledPreferenceKey -int 1
+#defaults write com.apple.safari WebKitTabToLinksPreferenceKey -int 1
+
+# Dock
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+# Set the icon size of Dock items to 48 pixels
+defaults write com.apple.dock tilesize -int 48
+
+# Kill affected applications
+for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
+	"Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
+	"Opera" "Safari" "SizeUp" "Spectacle" "SystemUIServer" "Terminal" \
+	"Transmission" "Twitter" "iCal"; do
+	killall "${app}" > /dev/null 2>&1
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
+
+# TODO chmod itunes
