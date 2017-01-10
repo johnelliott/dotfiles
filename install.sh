1c00f2bf656b60cb3b78ccf5977e9e238a1338a0
John Elliott <johnelliott703@gmail.com>
2017-01-09 21:36:45 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -2,34 +2,33 @@
 
 cd "$(dirname "${BASH_SOURCE}")" || exit
 
-#function removeOldFiles() {
-#	for file in ~/.{aliases,bash_profile,bash_prompt,bashrc,curlrc,exports,gitconfig,gitignore,hushlogin,inputrc,path,tmux.conf,wgetrc,zshrc,hub-completion.sh,git-completion.sh}; do
-#		rm "$file" >/dev/null 2>&1
-#	done
-#}
-#
-#function copyNewFiles() {
-#	cp .bash_profile ~/.bash_profile
-#	cp .bashrc ~/.bashrc
-#	cp .vim ~/.vimrc
-#	ln -s .gitconfig ~/.gitconfig
-#}
-#
-#function doIt() {
-#	removeOldFiles
-#	copyNewFiles
-#	source ~/.bash_profile
-#}
-#
-#if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
-#	doIt
-#else
-#	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1 -r
-#	echo
-#	if [[ $REPLY =~ ^[Yy]$ ]]; then
-#		doIt
-#	fi
-#fi
+function removeOldFiles() {
+	for file in ~/.{aliases,bash_profile,bash_prompt,bashrc,curlrc,exports,gitconfig,gitignore,hushlogin,inputrc,path,tmux.conf,wgetrc,zshrc,hub-completion.sh,git-completion.sh}; do
+		rm "$file" >/dev/null 2>&1
+	done
+}
+
+function copyNewFiles() {
+	cp .bash_profile ~/.bash_profile
+	cp .bashrc ~/.bashrc
+	cp .gitconfig ~/.gitconfig
+}
+
+function doIt() {
+	removeOldFiles
+	copyNewFiles
+	source ~/.bash_profile
+}
+
+if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
+	doIt
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt
+	fi
+fi
 
 # Install Homebrew on OS X
 if [[ $(uname) == 'Darwin' ]]; then
@@ -64,3 +63,11 @@ if which npm > /dev/null; then
 	fi
 fi
 
+# Install vim config
+if which npm > /dev/null; then
+  read -p "Would you like to install .vimrc? (y/n) " -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    curl -o- https://raw.githubusercontent.com/johnelliott/dotvim/master/.vimrc > ~/.vimrc
+  fi
+fi
