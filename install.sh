a456411fa95602a26391004d1546905a37534d8b
John Elliott <johnelliott703@gmail.com>
2017-01-10 18:23:57 -0500

    Update install script

--- install.sh
+++ install.sh
@@ -3,46 +3,52 @@
 cd "$(dirname "${BASH_SOURCE}")" || exit
 
 function removeOldFiles() {
-	for file in ~/.{aliases,bash_profile,bash_prompt,bashrc,curlrc,exports,gitconfig,gitignore,hushlogin,inputrc,path,tmux.conf,wgetrc,zshrc,hub-completion.sh,git-completion.sh}; do
-		rm "$file" >/dev/null 2>&1
-	done
+    for file in ~/.{aliases,bash_profile,bash_prompt,bashrc,curlrc,exports,gitconfig,gitignore,hushlogin,inputrc,path,tmux.conf,wgetrc,zshrc,hub-completion.sh,git-completion.sh}; do
+        rm "$file" >/dev/null 2>&1
+    done
 }
 
 function copyNewFiles() {
-	cp .bash_profile ~/.bash_profile
-	cp .bashrc ~/.bashrc
-	cp .gitconfig ~/.gitconfig
+    cp .bash_profile ~/.bash_profile
+    cp .bashrc ~/.bashrc
+    cp .gitconfig ~/.gitconfig
 }
 
 function doIt() {
-	removeOldFiles
-	copyNewFiles
-	source ~/.bash_profile
+    removeOldFiles
+    copyNewFiles
+    source ~/.bash_profile
 }
 
 if [ "$1" == "--force" ] || [ "$1" == "-f" ]; then
-	doIt
+    doIt
 else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1 -r
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
+    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        doIt
+    fi
 fi
 
 # Install Homebrew on OS X
 if [[ $(uname) == 'Darwin' ]]; then
-	read -p "Would you like to set up Homebrew? (y/n) " -n 1 -r
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		./brew.sh
-	fi
+    read -p "Would you like to set up Homebrew? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        ./brew.sh
+    fi
 
-	read -p "Would you like to set up OS X preferences? (y/n) " -n 1 -r
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		./osx.sh
-	fi
+    read -p "Would you like to set up GUI APPS via brew cask? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        ./brew-cask.sh
+    fi
+
+    read -p "Would you like to set up OS X preferences? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        ./macOS.sh
+    fi
 fi
 
 # Install node version manager
@@ -56,11 +62,11 @@ fi
 
 # Install npm packages
 if which npm > /dev/null; then
-	read -p "Would you like to install npm packages? (y/n) " -n 1 -r
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		./npm.sh
-	fi
+    read -p "Would you like to install npm packages? (y/n) " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        ./npm.sh
+    fi
 fi
 
 # Install vim config
