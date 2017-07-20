6976d224ae1e34988c334e3fa180a048ceb5a2c1
John Elliott <johnelliott703@gmail.com>
2017-07-19 22:17:56 -0400

    Clean up change-computer-name

--- change-computer-name.sh
+++ change-computer-name.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-# Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "diffie"
-sudo scutil --set HostName "diffie"
-sudo scutil --set LocalHostName "diffie"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "diffie"
+name='pokey'
 
+# Set computer name (as done via System Preferences → Sharing)
+sudo scutil --set ComputerName $name
+sudo scutil --set HostName $name
+sudo scutil --set LocalHostName $name
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $name
