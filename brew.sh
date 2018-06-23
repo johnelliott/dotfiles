d57e772bbb145b3726812bb0e5cec530702c4ae4
John Elliott <johnelliott703@gmail.com>
2018-06-22 20:12:24 -0400

    homebrew reality check

--- brew.sh
+++ brew.sh
@@ -18,51 +18,62 @@ fi
 brew doctor
 brew update
 
-# Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
 # Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
 brew install bash
 brew tap homebrew/versions
 
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
 brew install ack
+brew install ansible
 brew install awscli
+brew install bash
 brew install bash-completion@2
 brew install certbot
 brew install couchdb
+brew install exiftool
 brew install ext2fuse
 brew install ext4fuse
+brew install ffmpeg
+brew install findutils # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install fzf
 brew install git
 brew install git-lfs
+brew install gnu-sed --with-default-names # Install GNU `sed`, overwriting the built-in `sed`.
+brew install goaccess
+brew install graphicsmagick
 brew install grep
 brew install htop
+brew install hub
 brew install hub # https://github.com/github/hub
 brew install imagemagick --with-webp
 brew install jq
+brew install moreutils
+brew install netcat
+brew install nginx
 brew install nmap
 brew install openssh
+brew install openssl
 brew install postgresql
-brew install redis
-brew install screen
-brew install ssh-copy-id
+brew install pup
+brew install rsync
+brew install speedtest-cli
+brew install sqlite
 brew install ssh-copy-id
 brew install the_silver_searcher
 brew install tmux
+brew install transmission
 brew install tree
 brew install vim --with-override-system-vi
 brew install wget --with-iri
 brew install youtube-dl
+
 
 # Remove outdated versions from the cellar.
 brew cleanup
