2cbfad5f877166bc08fc71bd4d58148868116e3a
John Elliott <johnelliott703@gmail.com>
2024-05-14 15:32:00 -0700

    Update brew cask
    Removed items I may never use again
    
    RIP Atom

--- brew-cask.sh
+++ brew-cask.sh
@@ -6,21 +6,4 @@ if [[ ! "$(type -P brew)" ]]; then
 	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-#brew cask install atom
-#brew cask install cyberduck
-#brew cask install firefox
-#brew cask install google-chrome
-#brew cask install google-cloud-sdk
-#brew cask install macvim
-#brew cask install postgres
-#brew cask install slack
-#brew cask install spectacle
-#brew cask install vlc
-brew cask install qlcolorcode
-brew cask install qlstephen
-brew cask install qlmarkdown
-brew cask install quicklook-json
-brew cask install qlimagesize
-brew cask install webpquicklook
-brew cask install suspicious-package
-brew cask install qlvideo
+brew install --cask macvim rectangle vlc firefox
