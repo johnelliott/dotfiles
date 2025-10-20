78138de6622e9507fbe8276b522a4fcb167547ab
John Elliott <johnelliott@users.noreply.github.com>
2025-10-20 15:06:04 -0700

    Remove old stuff

--- brew-cask.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Make sure homebrew is installed first
-if [[ ! "$(type -P brew)" ]]; then
-	echo "Installing Homebrew"
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
-brew install --cask macvim rectangle vlc firefox
