51b6286753bb0ac46946fd39c34036816fb16154
John Elliott <johnelliott@users.noreply.github.com>
2025-10-29 15:30:07 -0700

    Try lualine

--- nvim/lua/plugins/general.lua
+++ nvim/lua/plugins/general.lua
@@ -3,39 +3,16 @@ return {
 	-- { "folke/trouble.nvim", enabled = false },
 	-- disable trouble
 	-- { "folke/snacks.nvim", enabled = false },
-	{ "johnelliott/vim-kinesis-kb900" },
+	-- { "johnelliott/vim-kinesis-kb900" },
 	{ "akinsho/bufferline.nvim", enabled = false },
-	{ "nvim-lualine/lualine.nvim", enabled = false },
+	{ "nvim-lualine/lualine.nvim", enabled = true },
 	{ "folke/noice.nvim", enabled = false },
 	{ "fatih/vim-go" },
 	{ "folke/flash.nvim", enabled = false },
-
-	-- I thought I was going to need to disable this for hidden text, but learned that conceallevle is a thing now
-	-- { "iamcco/markdown-preview.nvim", enabled = false },
-	-- { "markdown-preview.nvim", enabled = false },
-	-- {
-	-- 	"MeanderingProgrammer/render-markdown.nvim",
-	-- 	enabled = false,
-	-- 	opts = {
-	-- 		code = {
-	-- 			sign = false,
-	-- 			width = "block",
-	-- 			right_pad = 1,
-	-- 		},
-	-- 		heading = {
-	-- 			sign = false,
-	-- 			icons = {},
-	-- 		},
-	-- 		checkbox = {
-	-- 			enabled = false,
-	-- 		},
-	-- 	},
-	-- },
-	-- Disable inlay_hints
 	{
 		"nvim-lspconfig",
 		opts = {
-			inlay_hints = { enabled = false },
+			inlay_hints = { enabled = true },
 		},
 	},
 	{
@@ -57,7 +34,7 @@ return {
 				animate = { enabled = false },
 			},
 			bufferline = { enabled = false },
-			lualine = { enabled = false },
+			lualine = { enabled = true },
 			keys = {
 				{ "<leader>fF", LazyVim.pick("files"), desc = "Find Files (Root Dir)" },
 				{ "<leader>ff", LazyVim.pick("files", { root = false }), desc = "Find Files (cwd)" },
