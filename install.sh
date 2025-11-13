707d104155cccd411a4f93951ced6b31e501a350
johnelliott <johnelliott@users.noreply.github.com>
2025-11-12 16:08:58 -0800

    wip

--- install.sh
+++ install.sh
@@ -13,3 +13,16 @@ esac
 # Homebrew installs
 # https://docs.brew.sh/Brew-Bundle-and-Brewfile
 brew bundle check || brew bundle install
+
+# use gnu-stow to install
+stow -t ~ tmux
+stow -t ~ vim
+stow -t ~ readline
+stow -t ~ curl
+stow -t ~ git
+stow -t ~ ripgrep
+stow -t ~ wget
+mkdir -p ~/.config/nvim
+stow -t ~/.config/nvim nvim
+mkdir -p ~/.config/ghostty
+stow -t ~/.config/ghostty ghostty
--- nvim/init.lua
+++ nvim/init.lua
@@ -943,6 +943,8 @@ require("lazy").setup({
 				--
 				-- See :h blink-cmp-config-keymap for defining your own keymap
 				preset = "default",
+				["<C-k>"] = false, -- or {}, let digraph use be normal and don't break it plz
+				["<C-K>"] = { "show_signature", "hide_signature", "fallback" }, -- put blink behind capital ctrl-k
 
 				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
 				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
